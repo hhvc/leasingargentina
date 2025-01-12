85fe43aaf2b539d17664c58f4ab317210a9e98b7
hhvc <hectorvazquez.laboral@gmail.com>
2025-01-12 10:03:21 -0300

    agrego .env e icono ws

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 
 function Footer() {
+  const whatsappNumber = import.meta.env.VITE_WHATSAPP_NUMBER;
+
   return (
     <footer className="footer text-center">
       <p>
         ¿Querés saber más? Envíanos un mail a{" "}
         <a href="mailto:info@leasingargentina.ar">info@leasingargentina.ar</a> o
-        un{" "}
+        usa whatsapp {" "}
         <a
-          href="https://wa.me/3515478785"
+          href={`https://wa.me/${whatsappNumber}`}
           target="_blank"
           rel="noopener noreferrer"
+          className="text-success fw-bold"
         >
-          WhatsApp
+          <i className="bi bi-whatsapp fs-3 whatsapp-icon"></i>
         </a>
         .
       </p>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,30 +7,27 @@ function Main() {
         Motivos por los que tu empresa debe aprovechar sí o sí esta herramienta
         financiera
       </h2>
-      <ul>
-        <li>
-          Permite adquirir bienes con el producido de los mismos: Financiación
-          de largo plazo con entrega inicial mínima.
-        </li>
-        <li>
-          Reducción del impuesto a las ganancias: Las cuotas mensuales (cánones)
-          se imputan como gasto (pérdida).
-        </li>
-        {/* <li>
-          El bien se incorpora al patrimonio de la empresa al finalizar el
-          contrato y ya devaluado, disminuyendo el impacto en Activos.
-        </li>
-        <li>
-          Facilita la incorporación o actualización de equipos, permitiendo que
-          las empresas cuenten con tecnología de punta.
-        </li> */}
-        <li>
-          Puedes asesorarte con profesionales registrándote en{" "}
-          <a href="https://financiado.com.ar/">financiado.com.ar</a> y
-          completando el{" "}
-          <a href="https://financiado.com.ar/newcredit">formulario</a>.
-        </li>
-      </ul>
+      <p>
+        Permite adquirir bienes con el producido de los mismos: Financiación de
+        largo plazo con entrega inicial mínima.
+      </p>
+      <p>
+        Reducción del impuesto a las ganancias: Las cuotas mensuales (cánones)
+        se imputan como gasto (pérdida).
+      </p>
+      {/* <p>
+        El bien se incorpora al patrimonio de la empresa al finalizar el
+        contrato y ya devaluado, disminuyendo el impacto en Activos.
+      </p>
+      <p>
+        Facilita la incorporación o actualización de equipos, permitiendo que
+        las empresas cuenten con tecnología de punta.
+      </p> */}
+      <p>
+        Puedes asesorarte con profesionales registrándote en{" "}
+        <a href="https://financiado.com.ar/">financiado.com.ar</a> y completando
+        el <a href="https://financiado.com.ar/newcredit">formulario</a>.
+      </p>
     </main>
   );
 }
